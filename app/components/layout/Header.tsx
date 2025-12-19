5b26f0e899caaad8c18e276241ad8f5b044bde45
bangleskiduniya-prog <bangleskiduniya@gmail.com>
2025-12-19 12:26:50 +0500

    Delete app/components directory

--- app/components/layout/Header.tsx
+++
@@ -1,7 +0,0 @@
-import { Navbar } from "@/components/layout/Navbar";
-import { getCategories } from "@/lib/db/categories";
-
-export async function Header() {
-  const categories = await getCategories();
-  return <Navbar categories={categories} />;
-}
